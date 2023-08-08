06a56efafdba8cf0faa7bc3d3b9ef1dffe77bb93
jp3pe <45834750+jp3pe@users.noreply.github.com>
2023-08-08 16:18:02 +0900

    feature: top-nav template for bulletin board
    resolved #1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @GetMapping("/")
+    String home() {
+        return "index";
+    }
 
 }
